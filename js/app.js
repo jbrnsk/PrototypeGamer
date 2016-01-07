2ff717f41744ae23cba27e55057354f6bcba4ee7
jbrnsk <jbrnsk@gmail.com>
2016-01-07 08:42:44 -0500

    Haven't Pushed Changes. Oops.
    This change creates a Tabletop save file when the user hits save. The
    save file is currently using a default image from imgur to generate a
    deck of cards. Next step is to upload the image the user creates to
    imgur, and use that as a the image for the save file instead.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 
 window.onload = function()  {
   var canvas_1 = document.getElementsByTagName('canvas')[0];
-  canvas_1.width  = 320;
-  canvas_1.height = 320;
+  canvas_1.width  = 250;
+  canvas_1.height = 350;
 
   var canvas_2 = document.getElementsByTagName('canvas')[1];
-  canvas_2.width  = 320;
-  canvas_2.height = 320;
+  canvas_2.width  = 250;
+  canvas_2.height = 350;
 }
